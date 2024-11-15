5835b43d706906f1bd6725ac7e3d04e2d02140c2
nlok5923 <nlokhande5923@gmail.com>
2024-11-16 01:32:02 +0700

    feat: maintain leases

--- contracts/contracts/Vault.sol
+++ contracts/contracts/Vault.sol
@@ -7,7 +7,16 @@ contract Vault {
 
     uint64 public TOTAL_LIQUIDITY;
     uint64 public MAX_WITHDRAWABLE_LIQUIDITY = 1000000;
+    uint64 public noOfLeases = 0;
 
+    struct SolverLease {
+        uint256 duration;
+        uint64 amount;
+        address tokenAddress;
+        bool status;
+    }
+
+    mapping(uint64 => SolverLease) public leases;
     mapping(address => uint64) public loaners;
 
     // called by offchain cron job which will pull tokens from user
@@ -18,18 +27,24 @@ contract Vault {
 
     // called by solver
     function lease(uint256 _duration, uint64 _amount, address _tokenAddress) public {
-        // uint64 _prevLease = loaners[msg.sender];
-        // require(TOTAL_LIQUIDITY >= _amount, "Not enough liquidity");
-        // require(_prevLease + _amount <= MAX_WITHDRAWABLE_LIQUIDITY, "Amount exceeds max withdrawable liquidity");
+        uint64 _prevLease = loaners[msg.sender];
+        require(TOTAL_LIQUIDITY >= _amount, "Not enough liquidity");
+        require(_prevLease + _amount <= MAX_WITHDRAWABLE_LIQUIDITY, "Amount exceeds max withdrawable liquidity");
         
-        // TOTAL_LIQUIDITY -= _amount;
-        // IERC20(_tokenAddress).transfer(msg.sender, _amount);
+        TOTAL_LIQUIDITY -= _amount;
+        IERC20(_tokenAddress).transfer(msg.sender, _amount);
         
-        // loaners[msg.sender] += _amount;
+        loaners[msg.sender] += _amount;
+        leases[noOfLeases] = SolverLease(_duration, _amount, _tokenAddress, true);
+        noOfLeases++;
     } 
 
     // called by offchain cron job which will pay user his share back
     function payback() public {
 
+    }
+
+    function getTotalNoOfLeases() public view returns (uint256) {
+        return noOfLeases;
     }
 }
